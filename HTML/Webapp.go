e5697195f4f87f4b0619d03b38dd9e8c9f5df419
hil845035 <46377315+hil845035@users.noreply.github.com>
2019-01-07 13:56:48 -0500

    Add files via upload

---
+++ HTML/Webapp.go
@@ -0,0 +1,13 @@
+// static-files.go
+package main
+
+import "net/http"
+
+func main() {
+fs := http.FileServer(http.Dir("assets/"))
+http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+http.ListenAndServe(":8080", nil)
+}
+
+
