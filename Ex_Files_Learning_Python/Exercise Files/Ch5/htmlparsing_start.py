bb336e7e83f2d19cf9d3a430bc1d0ad665be8a5f
hil845035 <46377315+hil845035@users.noreply.github.com>
2019-01-07 13:31:37 -0500

    Add files via upload

---
+++ Ex_Files_Learning_Python/Exercise Files/Ch5/htmlparsing_start.py
@@ -0,0 +1,19 @@
+# 
+# Example file for parsing and processing HTML
+#
+from html.parser import HTMLPatser
+
+class MyHTMLParser(MyHTMLParser):
+
+
+def main():
+  # instantiate the parser and feed it some HTML
+  parser = MyHTMLParser()
+  f = open("samplehtml.html")
+  if f.mode == 'r'
+    contents = f.read()
+    
+
+if __name__ == "__main__":
+  main();
+  
